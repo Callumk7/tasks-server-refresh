28abaa47167578043ea7c463f5abceaea5e370e8
Callum Kloos <callumkloos@gmail.com>
2023-03-22 18:26:14 +0000

    added a /tasks route using prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import express, { Express, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 import { config } from "dotenv";
 
 config();
-
+const prisma = new PrismaClient();
 const app: Express = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Express server running Typescript");
+});
+
+app.get("/tasks", async (req: Request, res: Response) => {
+	const allTasks = await prisma.task.findMany();
+	res.json(allTasks);
 });
 
 app.listen(port, () => {
