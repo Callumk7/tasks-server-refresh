04b34b26b7080da43748563505065b267913481a
Callum Kloos <callumkloos@gmail.com>
2023-03-22 17:51:39 +0000

    src directory setup

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,9 @@ const app: Express = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express server running Typescript");
+	res.send("Express server running Typescript");
 });
 
 app.listen(port, () => {
-  console.log(
-    `I just changed this message, and the server is still running on port ${port}`
-  );
+	console.log(`Server is running on port ${port}`);
 });
