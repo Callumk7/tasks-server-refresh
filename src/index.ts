f48d02e703952a3892fb399e6f94f7312fa7eae9
Callum Kloos <callumkloos@gmail.com>
2023-03-22 19:53:16 +0000

    fixed some problems with source directory

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express, { Express, Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import { config } from "dotenv";
+import prisma from "./client";
 
 config();
-const prisma = new PrismaClient();
 const app: Express = express();
 const port = process.env.PORT || 8080;
 
@@ -14,6 +14,28 @@ app.get("/", (req: Request, res: Response) => {
 app.get("/tasks", async (req: Request, res: Response) => {
 	const allTasks = await prisma.task.findMany();
 	res.json(allTasks);
+});
+
+app.get("/tasks/:id", async (req: Request, res: Response) => {
+	const { id } = req.params;
+	if (!id) {
+		res.status(400).json({ error: "id is required" });
+		return;
+	}
+	if (isNaN(Number(id))) {
+		res.status(400).json({ error: "id must be a number" });
+		return;
+	}
+	const task = await prisma.task.findUnique({
+		where: {
+			id: Number(id),
+		},
+	});
+	if (!task) {
+		res.status(404).json({ error: "task not found" });
+		return;
+	}
+	res.json(task);
 });
 
 app.listen(port, () => {
