ff2cc916c8b2e1bdee1e8a618a90daf1fe9d38d6
Callum Kloos <callumkloos@gmail.com>
2023-03-25 16:08:12 +0000

    LOGGING?

--- src/middleware/logging.ts
+++ src/middleware/logging.ts
@@ -1,6 +1,18 @@
 import { Request, Response, NextFunction } from "express";
+import prisma from "../client";
 
-export const logger = (req: Request, res: Response, next: NextFunction) => {
-	console.log(`${req.method} ${req.path}`);
+export const logger = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
 	next();
+	const { method, path } = req;
+	const event = await prisma.event.create({
+		data: {
+			type: method,
+			route: path,
+		},
+	});
+	console.log(event);
 };
