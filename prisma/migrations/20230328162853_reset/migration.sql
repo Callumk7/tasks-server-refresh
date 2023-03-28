35e2639a3b2a3e4a92516500d8caccb7800f4e2f
Callum Kloos <callumkloos@gmail.com>
2023-03-28 17:29:28 +0100

    timestamps?

--- prisma/migrations/20230328162853_reset/migration.sql
+++ prisma/migrations/20230328162853_reset/migration.sql
@@ -1,5 +1,3 @@
-yarn run v1.22.19
-$ /Users/callumkloos/Projects/react-tasks/tasks-server-refresh/node_modules/.bin/prisma migrate diff --from-empty --to-schema-datamodel prisma/schema.prisma --script
 -- CreateTable
 CREATE TABLE "Project" (
     "id" SERIAL NOT NULL,
@@ -8,6 +6,7 @@ CREATE TABLE "Project" (
     "archived" BOOLEAN NOT NULL DEFAULT false,
     "completed" BOOLEAN NOT NULL DEFAULT false,
     "deleted" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
 );
@@ -25,7 +24,16 @@ CREATE TABLE "Task" (
     CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" SERIAL NOT NULL,
+    "type" TEXT NOT NULL,
+    "route" TEXT NOT NULL,
+    "data" TEXT,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
+
 -- AddForeignKey
 ALTER TABLE "Task" ADD CONSTRAINT "Task_project_id_fkey" FOREIGN KEY ("project_id") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
-Done in 1.29s.
