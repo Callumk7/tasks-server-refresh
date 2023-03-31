572c58060ec0ad60f110f4f8a55eabf8f41e58cb
Callum Kloos <callumkloos@gmail.com>
2023-03-31 18:20:14 +0100

    On branch node-deployment Your branch is up to date with 'origin/node-deployment'.
    Changes to be committed:
    modified:   src/routes/tasks.ts

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,13 +1,7 @@
 import { Router } from "express";
-import {
-	createTask,
-	getAllTasks,
-	getTaskById,
-	updateTask,
-} from "../controllers/tasks";
+import { createTask, getAllTasks, updateTask } from "../controllers/tasks";
 
 export const tasksRouter = Router();
 tasksRouter.get("/", getAllTasks);
-tasksRouter.get("/:id", getTaskById);
 tasksRouter.post("/", createTask);
 tasksRouter.patch("/:id", updateTask);
