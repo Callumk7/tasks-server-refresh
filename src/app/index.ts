997e25c8037fd67f55cdb3147f647fec3bd6efee
Callum Kloos <callumkloos@gmail.com>
2023-03-31 15:18:52 +0100

    revert last change

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,5 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
-import asyncErrors from "express-async-errors";
 import { tasksRouter } from "../routes/tasks";
 import { projectsRouter } from "../routes/projects";
 import { logger } from "../middleware/logging";
@@ -8,7 +7,6 @@ import { logger } from "../middleware/logging";
 export const app: Express = express();
 
 app.use(cors()); // Enable CORS
-app.use(asyncErrors()); // Enable async error handling
 app.use(express.json()); // Parse JSON bodies
 app.use(logger); // Log all requests
 
